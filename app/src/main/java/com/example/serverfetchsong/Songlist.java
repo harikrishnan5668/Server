a096c931a2c8d06bb404e3d0b56fc005c67806e4
harikrishnan5668 <harizzzz0007@gmail.com>
2021-02-03 22:24:10 +0530

    server

---
+++ app/src/main/java/com/example/serverfetchsong/Songlist.java
@@ -0,0 +1,14 @@
+package com.example.serverfetchsong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Songlist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_songlist);
+    }
+}
